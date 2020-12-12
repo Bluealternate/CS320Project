eebe3f1995ff7451ad61fbec3f32eaa6734f5e2f
Bluealternate <vinh.duong@wsu.edu>
2020-12-11 22:43:15 -0800

    Worked on FeedAnimal

--- WorkFolder/Javascript/ArrayHolder.js
+++ WorkFolder/Javascript/ArrayHolder.js
@@ -1,4 +1,5 @@
 // Javascript file to contain our files
+// https://buzzcoder.gitbooks.io/codecraft-javascript/content/object/iterate-over-a-dictionary.html
 class Animal {
   constructor(name, type, description) {
     this.name = name;
@@ -9,24 +10,39 @@ class Animal {
 
 const Cat1 = new Animal('Fluffy', 'Cat', 'A cat that is really fluffy');
 const Dog1 = new Animal('Woof', 'Dog', 'A dog that says woof');
-const arrayForUser1 = [];
-arrayForUser1.push(Cat1);
-arrayForUser1.push(Dog1);
+const arrayForUser1 = {};
+arrayForUser1[Cat1.name] = Cat1;
+arrayForUser1[Dog1.name] = Dog1;
 
 const Monkey1 = new Animal('Tail', 'Monkey', 'Eating a banana');
 const Giraffe2 = new Animal('LongNeck', 'Giraffe', 'Has a long neck');
-const arrayForUser2 = [];
-arrayForUser2.push(Monkey1);
-arrayForUser2.push(Giraffe2);
+const arrayForUser2 = {};
+arrayForUser2[Monkey1.name] = Monkey1;
+arrayForUser2[Giraffe2.name] = Giraffe2;
 
 function ListAnimalForFeedAnimal() { // Normally use for loop and clean the code up more.
+  let numberTracker = 1;
   if (document.cookie === '1') {
-    document.getElementById('1').innerHTML = arrayForUser1[0].name;
-    document.getElementById('2').innerHTML = arrayForUser1[1].name;
+    for (let animals in arrayForUser1) {
+      document.getElementById(numberTracker.toString()).innerHTML = arrayForUser1[animals].name;
+      numberTracker += 1;
+    }
+
   } else if (document.cookie === '2') {
-    document.getElementById('1').innerHTML = arrayForUser2[0].name;
-    document.getElementById('2').innerHTML = arrayForUser2[1].name;
+    for (let animals in arrayForUser2) {
+      document.getElementById(numberTracker.toString()).innerHTML = arrayForUser2[animals].name;
+      numberTracker += 1;
+    }
   } else {
     console.log('Error');
   }
+}
+
+function goToLink(idValue) {
+  document.cookie = `${document.cookie} ${document.getElementById(idValue).innerHTML}`;
+  window.location.href = 'FeedAnimalSupplement.html';
+}
+
+function showAnimal() {
+  document.getElementById('AnimalPicture').src = '../Images/FeedAnimalsImages/Cat1.png';
 }
--- WorkFolder/Javascript/FeedAnimalJavascript.js
+++ WorkFolder/Javascript/FeedAnimalJavascript.js
@@ -1,9 +1,12 @@
+// https://dmitripavlutin.com/string-interpolation-in-javascript/
+
 let animalFeedCount = 0;
 
 function feedAnimal() {
   animalFeedCount += 1;
   if (animalFeedCount === 3) {
     // Used https://www.w3schools.com/jsref/prop_img_src.asp to find out.
-    document.getElementById('AnimalPicture').src = '../Images/FeedAnimalsImages/Rabbit2.png';
+    var animalName = 'Rabbit';
+    document.getElementById('AnimalPicture').src = `../Images/FeedAnimalsImages/${animalName}2.png`;
   }
 }
