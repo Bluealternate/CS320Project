eab6b61ec4a0d0a00030c4d27027dd218034cc5b
IneptMantis4061 <john191410@gmail.com>
2020-12-11 22:17:19 -0800

    added picture change for create animal page

--- WorkFolder/Javascript/CreateAnimalPage.js
+++ WorkFolder/Javascript/CreateAnimalPage.js
@@ -2,6 +2,7 @@ var animal = "dog";
 var name = "Fido";
 var ball;
 var house;
+var randomNames = ["Kevin", "Fido", "Randy", "Alex", "Fluffy", "Princess", "Doug", "Bubbles"];
 
 //this code should construct an animal with the desired attributes the user has requested
 function createAnimal(){
@@ -12,12 +13,12 @@ function createAnimal(){
 //this function should randomly set the name field for the animal to be created
 function randomNameGenerator(){
   //get a random number
-
-  //use modulo operator to fit the random number to the index range of our random list of names
+  var random = Math.floor(Math.random() * Math.floor(randomNames.length - 1));
 
   //use number to fetch a name from the list
-
   //set the name field for the animal to be created to the fetched name
+  name = randomNames[random];
+  document.getElementById("animalNameTextBox").value = name;
 }
 
 //this function should store the type of ball the user would like for their pet
@@ -35,4 +36,6 @@ function storeHome(id){
 function storeAnimal(id){
   animal = id;
   //console.log(animal);
+
+  document.getElementById("animalPic").src = document.getElementById(id + " pic").src;
 }
