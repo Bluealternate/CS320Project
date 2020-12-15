2b15dcfe6fbb9d6ebc257daa0a273a9c6d47c847
IneptMantis4061 <john191410@gmail.com>
2020-12-14 21:46:01 -0800

    added selected feature for create animal page

--- WorkFolder/Javascript/CreateAnimalPage.js
+++ WorkFolder/Javascript/CreateAnimalPage.js
@@ -3,6 +3,8 @@ var name = "Fido";
 var ball;
 var house;
 var randomNames = ["Kevin", "Fido", "Randy", "Alex", "Fluffy", "Princess", "Doug", "Bubbles"];
+var table;
+var cells;
 
 //this code should construct an animal with the desired attributes the user has requested
 function createAnimal(){
@@ -22,20 +24,40 @@ function randomNameGenerator(){
 }
 
 //this function should store the type of ball the user would like for their pet
-function storeBall(id){
+function storeBall(id) {
   ball = document.getElementById(id).id;
   //console.log(ball);
 
-  //document.getElementById(id).style.border = ;
+  document.getElementById(id).parentNode.backgroundColor = "white";
+
+  document.getElementById(id).style.backgroundColor = "yellow";
+
+  let table = document.getElementById('ball_table');
+  let cells = table.getElementsByTagName('td');
+
+  for (let cell = 0; cell < cells.length; cell++) {
+    cells[cell].style.backgroundColor = "";
+  }
+  let cellSelected = table.getElementsByTagName('td')[id];
+  cellSelected.style.backgroundColor = "yellow";
 }
 
 //this function should store the type of house the user would like for their pet
 function storeHome(id){
   house = document.getElementById(id).id;
   //console.log(house);
+
+  let table = document.getElementById('house_table');
+  let cells = table.getElementsByTagName('td');
+
+  for (let cell = 0; cell < cells.length; cell++) {
+    cells[cell].style.backgroundColor = "";
+  }
+  let cellSelected = table.getElementsByTagName('td')[id];
+  cellSelected.style.backgroundColor = "yellow";
 }
 
-function storeAnimal(id){
+function storeAnimal(id) {
   animal = id;
   //console.log(animal);
 
