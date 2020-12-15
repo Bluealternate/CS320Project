3333fc42463db70f79fe47672c764511484c33c0
slanante1997 <73133434+slanante1997@users.noreply.github.com>
2020-12-15 11:21:16 -0800

    Added testSuite
    Added working testSuite w/mocha chai
    Seth L

---
+++ WorkFolder/Javascript/tests.js
@@ -0,0 +1,7 @@
+describe('Animal Database Tests', function () {
+  describe('Timeout Test', function () {
+    it('Random test to see if testSuiteworks', function (){
+      this.timeout(50);
+    });
+  });
+});
