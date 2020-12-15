cca737cdac2535089601b0aa48e2ecc19e9e711f
IneptMantis4061 <john191410@gmail.com>
2020-12-15 11:26:03 -0800

    added sessionstorage upload on login

--- WorkFolder/Javascript/LogIn.js
+++ WorkFolder/Javascript/LogIn.js
@@ -8,12 +8,35 @@ function checkCredentials() {
   if (username === 'a' && password === 'a') {
     document.cookie = '1';
     sessionStorage.setItem("user", "a");
+    loadAnimals("a");
     window.location.href = 'MainPage.html';
   } else if (username === 'b' && password === 'b') {
     document.cookie = '2';
     sessionStorage.setItem("user", "b");
+    loadAnimals("b");
     window.location.href = 'MainPage.html';
   } else {
     window.location.href = 'LogIn.html';
   }
+}
+
+function loadAnimals(user) {
+  console.log(localStorage.length);
+  if(localStorage.length === 0) {
+    return;
+  }
+
+  for(let i = 0; i < localStorage.length; i++){
+    if((JSON.parse(localStorage.getItem(localStorage.key(i)))[4]) === user){
+      console.log("loading into temporary memory");
+      sessionStorage.setItem(JSON.parse(localStorage.getItem(localStorage.key(i)))[0] ,localStorage.getItem(localStorage.key(i)));
+    }
+  }
+
+  /*
+  for(let i = 0; i < sessionStorage.length; i++){
+   console.log(sessionStorage.getItem(sessionStorage.key(i)));
+  }
+  */
+
 }
