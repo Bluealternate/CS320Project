3df4445ad43d325400e00edb3210698a5c4ba8c1
Bluealternate <vinh.duong@wsu.edu>
2020-12-16 15:24:06 -0800

    Cleanred up the javascript code.

--- app/Javascript/ArrayHolder.js
+++ app/Javascript/ArrayHolder.js
@@ -1,10 +1,12 @@
 // Javascript file to contain our files
 // https://buzzcoder.gitbooks.io/codecraft-javascript/content/object/iterate-over-a-dictionary.html
+// For array Destructuring error
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
 // Feed Animal helper variables
-let feedAnimalHelperName = '';
+let feedAnimalHelperName = ''; // Helper variable used in the feed animal page.
 const animalz = [100];
 let track = 0;
-class Animal {
+class Animal { // To create an animal object
   constructor(name, type, description) {
     this.name = name;
     this.type = type;
@@ -27,7 +29,7 @@ function build() {
     i++;
   }
 }
-
+// Create some instances of the animals and put then into dictionaries.
 build();
 const Cat1 = new Animal('Fluffy', 'Cat', 'A cat that is really fluffy');
 animalz[track] = Cat1;
@@ -49,42 +51,44 @@ const arrayForUser2 = {};
 arrayForUser2[Monkey1.name] = Monkey1;
 arrayForUser2[Giraffe2.name] = Giraffe2;
 
-function ListAnimalForFeedAnimal() { // Normally use for loop and clean the code up more.
-  let numberTracker = 1;
-  if (document.cookie === '1') {
-    for (const animals in arrayForUser1) {
-      document.getElementById(numberTracker.toString()).innerHTML = arrayForUser1[animals].name;
-      numberTracker += 1;
+function ListAnimalForFeedAnimal() { // To list the user animals in the Feed Animal page
+  let numberTracker = 1; // Helper variable to list the animals.
+  if (document.cookie === '1') { // If the user is user A
+    for (const animals in arrayForUser1) { // Go through all of user A animals
+      document.getElementById(numberTracker.toString()).innerHTML = arrayForUser1[animals].name; // Display name
+      numberTracker += 1; // Increment helper number
     }
-  } else if (document.cookie === '2') {
-    for (const animals in arrayForUser2) {
-      document.getElementById(numberTracker.toString()).innerHTML = arrayForUser2[animals].name;
-      numberTracker += 1;
+  } else if (document.cookie === '2') { // If the user is user B
+    for (const animals in arrayForUser2) { // Go through all of user B animals
+      document.getElementById(numberTracker.toString()).innerHTML = arrayForUser2[animals].name; // Display name
+      numberTracker += 1; // Increment helper number
     }
-  } else {
+  } else { // This part should not run and used for error checking potentially.
     console.log('Error');
   }
 }
 
-function goToLink(idValue) {
-  document.cookie = `${document.cookie} ${document.getElementById(idValue).innerHTML}`;
+function goToLink(idValue) { // Go to a page to feed the animal if the animal name is clicked
+  document.cookie = `${document.cookie} ${document.getElementById(idValue).innerHTML}`; // Store animal name in cookie.
   window.location.href = 'FeedAnimalSupplement.html';
 }
 
-function showAnimal() {
-  const cookieSections = document.cookie.split(' ');
-  feedAnimalHelperName = cookieSections[1];
-  if (cookieSections[0] === '1') {
+function showAnimal() { // Show the picture of the animal to be fed
+  const cookieSections = document.cookie.split(' '); // get user number and animal name
+  const [y, z] = cookieSections;
+  feedAnimalHelperName = z;
+  if (cookieSections[0] === '1') { // If user A get the type of the animal being display and display the animal type
     const animalType = arrayForUser1[feedAnimalHelperName].type;
     document.getElementById('AnimalPicture').src = `../Images/FeedAnimalsImages/${animalType}1.png`;
-  } else if (cookieSections[0] === '2') {
+  } else if (cookieSections[0] === '2') {// If user B get the type of the animal and display animal type
     const animalType = arrayForUser2[feedAnimalHelperName].type;
     document.getElementById('AnimalPicture').src = `../Images/FeedAnimalsImages/${animalType}1.png`;
   }
-  document.cookie = cookieSections[0];
+  document.cookie = y; // Reassign the cookie so that it has the user number
 }
 
-function feedAnimalHelper() {
+function feedAnimalHelper() { // function to change the picture display to have stars the more times the animal is fed.
+  // For user A increment feed count and display appropiate picture and change rank based off of feedcount.
   if (document.cookie === '1') {
     arrayForUser1[feedAnimalHelperName].feedCount += 1;
     const feeds = arrayForUser1[feedAnimalHelperName].feedCount;
@@ -99,6 +103,7 @@ function feedAnimalHelper() {
       arrayForUser1[feedAnimalHelperName].rank = 3;
       document.getElementById('AnimalPicture').src = `../Images/FeedAnimalsImages/${animalType}4.png`;
     }
+    // For user B increment feed count and display appropiate picture and change rank based off of feedcount.
   } else if (document.cookie === '2') {
     arrayForUser2[feedAnimalHelperName].feedCount += 1;
     const feeds = arrayForUser2[feedAnimalHelperName].feedCount;
--- app/Javascript/LogIn.js
+++ app/Javascript/LogIn.js
@@ -2,23 +2,28 @@
 // https://stackoverflow.com/questions/2018567/loading-another-html-page-from-javascript
 // https://www.w3schools.com/js/js_if_else.asp    For information on elseif
 // https://www.w3schools.com/js/js_cookies.asp
+// https://www.w3schools.com/jsref/jsref_length_string.asp //To find out how to get the length of strings.
 function checkCredentials() {
-  const username = document.getElementById('username').value;
+  const username = document.getElementById('username').value; // Get the username and passwords
   const password = document.getElementById('password').value;
+  // If the username and password is long then just automatically reload the page
+  if (username.length >= 20 || password.length >= 30) {
+    window.location.href = 'LogIn.html';
+  }
 
   sessionStorage.clear();
 
-  if (username === 'a' && password === 'a') {
+  if (username === 'a' && password === 'a') { // If user A set the cookie to 1 and go to the main page
     document.cookie = '1';
     sessionStorage.setItem("user", "a");
     loadAnimals("a");
     window.location.href = 'MainPage.html';
-  } else if (username === 'b' && password === 'b') {
+  } else if (username === 'b' && password === 'b') { // If user B set the cookie to 2 and go to the main page
     document.cookie = '2';
     sessionStorage.setItem("user", "b");
     loadAnimals("b");
     window.location.href = 'MainPage.html';
-  } else {
+  } else { // Else reload the page because the credentials are wrong.
     window.location.href = 'LogIn.html';
   }
 }
