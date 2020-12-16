f6b5634743c66aca348ee4d764f5bcd525bb3450
slanante1997 <73133434+slanante1997@users.noreply.github.com>
2020-12-15 22:14:49 -0800

    Added comments to testSuite

--- WorkFolder/Javascript/tests.js
+++ WorkFolder/Javascript/tests.js
@@ -1,3 +1,5 @@
+/* Tests below are not proper tests, had difficulty implementing tests since javascript methods did not have return values */
+
 describe('Animal Database Tests', function () {
   describe('playWithAnimal Test', function () {
     it('Level Up Animal when Experience is >= 100, then reset XP to 0, level up is called four times to reach level 1', function (){
